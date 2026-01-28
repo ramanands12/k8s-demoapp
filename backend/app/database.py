724cf2f9a552e061c5091e9478eefe2f91609728
ramanands12 <ramanandsacharya@gmail.com>
2026-01-28 13:52:23 +0530

    Changed files

---
+++ backend/app/database.py
@@ -0,0 +1,17 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+
+def get_connection():
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        cursor_factory=RealDictCursor
+    )
