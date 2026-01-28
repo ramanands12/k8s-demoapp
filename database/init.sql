f61cfcd3d2350c56e36463c68c5f6d8ade67a7ac
ramanands12 <ramanandsacharya@gmail.com>
2026-01-28 15:36:17 +0530

    database file added

--- database/init.sql
+++ database/init.sql
@@ -3,7 +3,15 @@ CREATE TABLE users (
   name TEXT
 );
 
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  title TEXT,
+  completed BOOLEAN DEFAULT FALSE
+);
+
 INSERT INTO users (name) VALUES
 ('Ram'),
-('Kubernetes'),
-('DevOps');
+('DevOps'),
+('Kubernetes');
+
